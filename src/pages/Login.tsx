10a6b50e96be229f01dfa570a107f17c8043c5aa
adilououssei <adilououssei@gmail.com>
2026-06-24 20:14:45 +0000

    correction des erreurs

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -2,7 +2,7 @@ import { useState, type FormEvent } from 'react'
 import { useNavigate } from 'react-router-dom'
 import { motion } from 'framer-motion'
 import { useTranslation } from 'react-i18next'
-import { LogIn, Lock, Mail, Eye, EyeOff, Zap } from 'lucide-react'
+import { LogIn, Eye, EyeOff, Zap } from 'lucide-react'
 
 const Login = () => {
   const { t } = useTranslation()
